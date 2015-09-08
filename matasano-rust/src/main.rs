0dc76b10c66187a89f12fb82ca84aa55e6e169bc
dan <danoctavian91@gmail.com>
2015-09-08 02:44:49 -0700

    c1;s5

--- matasano-rust/src/main.rs
+++ matasano-rust/src/main.rs
@@ -5,6 +5,8 @@ use serialize::base64::{self, ToBase64};
 use serialize::hex::FromHex;
 use std::str;
 
+use serialize::hex::ToHex;
+
 use std::collections::HashMap;
 /// S1;C1
 fn hexToBase64(hexStr: &str) -> Result<String, serialize::hex::FromHexError> {
@@ -59,11 +61,20 @@ fn englishScore(s: &str) -> i64 {
 	let score: f64 = freqs.zip(freqVals.iter())
 												.map(|(x, y)| (num::pow(x - y, 2)))
 												.fold(0.0, |acc, item| acc + item).sqrt();
-	(score * 100.0) as i64 
+	(score * 1000.0) as i64 
 }
 
 // english letter frequencies
 const freqVals: [f64; 26] = [8.2, 1.5, 2.8, 4.3, 12.7, 2.2, 2.0, 6.1, 7.0, 0.2, 0.8, 4.0, 2.4, 6.7, 7.5, 2.0, 0.1, 6.0, 6.3, 9.1, 2.8, 1.0, 2.4, 0.2, 2.0, 0.1];
+
+
+/// S1;C5
+fn encryptRepeatXOR(plain: &str, key: &str) -> String {
+	let enc = plain.bytes().zip(key.bytes().cycle()).map(|(b, k)| b ^ k);
+
+	let bs: Vec<u8> = enc.collect();
+	(bs[..]).to_hex()
+}
 
 fn resultToOpt<T, E>(r: Result<T, E>) -> Option<T> {
 	match r {
@@ -74,10 +85,6 @@ fn resultToOpt<T, E>(r: Result<T, E>) -> Option<T> {
 
 fn main() {
 	println!("matasano crypto challenges...");
-	let i = (1..10).next();
-
-	//let v = Vec::<char>::from_iter(cs);
-	//println!("{}", v);
 }
 
 #[test]
@@ -100,4 +107,12 @@ fn testFixedXOR() {
 fn testFindKey() {
 	let (k, str) = singleCharXOR("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
 	assert_eq!(str, "Cooking MC's like a pound of bacon");
+}
+
+#[test]
+fn testEncryptRepeatXOR() {
+	let stanza = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+	let key = "ICE";
+	let actual = encryptRepeatXOR(&stanza, &key);								
+	assert_eq!(actual, "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f");
 }
